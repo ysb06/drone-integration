7f9223b8501e76d558bddc323855d86dceda3eb2
Seungbin Yim <ysb06@hotmail.com>
2020-08-04 00:35:14 +0900

    Add Scoreboard
    Signed-off-by: Seungbin Yim <ysb06@hotmail.com>

--- ts-react-frontend/src/components/pages/Pickit/scoreboard.tsx
+++ ts-react-frontend/src/components/pages/Pickit/scoreboard.tsx
@@ -2,36 +2,157 @@ import React, { Component } from 'react';
 import './scoreboard.css';
 import { RouteComponentProps } from 'react-router-dom';
 
+const Final_Round = 2;
+
 interface IScoreboardState {
     round: number,
+    turn: GameTurn,
+    targetTime?: Date,
     p1Time: Date,
     p2Time: Date,
     p1Score: number,
     p2Score: number
 }
 
+enum GameTurn {
+    P1, P2, End
+}
+
 class Scoreboard extends Component<RouteComponentProps, IScoreboardState> {
+    constructor(props: RouteComponentProps) {
+        super(props);
+
+        this.onChangeP1Score = this.onChangeP1Score.bind(this);
+        this.onChangeP2Score = this.onChangeP2Score.bind(this);
+        this.activateRound = this.activateRound.bind(this);
+        this.refreshBoard = this.refreshBoard.bind(this);
+        this.resetRound = this.resetRound.bind(this);
+    }
     state: IScoreboardState = {
         round: 1,
-        p1Time: new Date(1000),
-        p2Time: new Date(1000),
+        turn: GameTurn.End,
+        p1Time: new Date(180000),
+        p2Time: new Date(180000),
         p1Score: 0,
         p2Score: 0
     }
 
+    activateRound(event: React.MouseEvent<HTMLInputElement, MouseEvent>) {
+        if (this.state.round > Final_Round) {
+            alert('게임이 이미 종료되었습니다.');
+            return;
+        }
+
+        let currentTurn: GameTurn = this.state.turn;
+        if (currentTurn === GameTurn.End) {
+            currentTurn = GameTurn.P1;
+        }
+
+        this.setState({
+            turn: currentTurn,
+            targetTime: new Date(Date.now() + (this.state.round === 1 ? 10000 : 5000))
+        });
+        setTimeout(this.refreshBoard, 500);
+        console.log('Round ' + this.state.round + ', Player ' + (currentTurn + 1) + ' turn');
+    }
+
+    refreshBoard() {
+        let remainedTime: number = 0;
+        if(this.state.targetTime) {
+            remainedTime = this.state.targetTime.getTime() - Date.now();
+        } else {
+            alert('Time not set error');
+            return;
+        }
+        console.log('refresh!!: ' + remainedTime);
+
+        if(remainedTime > 0) {
+            switch (this.state.turn) {
+                case GameTurn.P1:
+                    this.setState({p1Time: new Date(remainedTime)});
+                    break;
+                case GameTurn.P2:
+                    this.setState({p2Time: new Date(remainedTime)});
+                    break;
+                default:
+                    alert('Unknown Turn error');
+                    return;
+            }
+            
+            setTimeout(this.refreshBoard, 500);
+        } else {
+            this.setState({
+                round: (this.state.turn < 1? this.state.round : this.state.round + 1),
+                turn: this.state.turn + 1
+            });
+        }
+    }
+
+    resetRound(event: React.MouseEvent<HTMLInputElement, MouseEvent>) {
+        this.setState({
+            round: 1,
+            turn: GameTurn.End
+        })
+    }
+
+    onChangeP1Score(event: React.ChangeEvent<HTMLInputElement>) {
+        let score = Number.parseInt(event.target.value);
+        if (isNaN(score)) {
+            this.setState({ p1Score: 0 });
+        } else {
+            this.setState({ p1Score: score });
+        }
+    }
+
+    onChangeP2Score(event: React.ChangeEvent<HTMLInputElement>) {
+        let score = Number.parseInt(event.target.value);
+        if (isNaN(score)) {
+            this.setState({ p2Score: 0 });
+        } else {
+            this.setState({ p2Score: score });
+        }
+    }
+
     render() {
         return (
-            <div>
-                <div className="round">{this.state.round}</div>
+            <div className="score-app">
                 <div className="time-board">
-                    <div className="p1-time">{this.state.p1Time.getMinutes() + ":" + this.state.p1Time.getSeconds()}</div>
-                    <div className="p2-time">{this.state.p2Time.getMinutes() + ":" + this.state.p2Time.getSeconds()}</div>
+                    <div className="time-content">
+                        <span className="score-text">
+                            {this.state.p1Time.getMinutes() + ":" +
+                                (this.state.p1Time.getSeconds() < 10 ? "0" + this.state.p1Time.getSeconds() : this.state.p1Time.getSeconds())}
+                        </span>
+                    </div>
+                    <div className="round">
+                        <span className="round-title">Round</span>
+                        <span className="round-content">{this.state.round > Final_Round? "종료" : this.state.round}</span>
+                    </div>
+                    <div className="time-content">
+                        <span className="score-text">
+                            {this.state.p2Time.getMinutes() + ":" +
+                                (this.state.p2Time.getSeconds() < 10 ? "0" + this.state.p2Time.getSeconds() : this.state.p2Time.getSeconds())}
+                        </span>
+                    </div>
                 </div>
                 <div className="score-board">
-                    <div className="divider">:</div>
-                    <div className="p1-score">{this.state.p1Score}</div>
-                    <div className="p2-score">{this.state.p2Score}</div>
+                    <div className="score-content">
+                        <span className="score-text">
+                            <input type="text" onChange={this.onChangeP1Score} value={this.state.p1Score} />
+                        </span>
+                    </div>
+                    <div className="divider">
+                        <span className="score-text">:</span>
+                    </div>
+                    <div className="score-content">
+                        <span className="score-text">
+                            <input type="text" onChange={this.onChangeP2Score} value={this.state.p2Score} />
+                        </span>
+                    </div>
                 </div>
+                <form className="control-panel">
+                    <input type="button" value="리셋" onClick={this.resetRound} />
+                    <input type="button" value="시작" onClick={this.activateRound} />
+                </form>
             </div>
         );
     }
