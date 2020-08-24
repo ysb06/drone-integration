1576b2213f16a2deeb5819d27fe58ea8a471547d
sbyim <ysb06@hotmail.com>
2020-08-25 06:45:58 +0900

    Fix scoreboard css and sound
    Signed-off-by: sbyim <ysb06@hotmail.com>

--- ts-react-frontend/src/components/pages/Pickit/scoreboard.tsx
+++ ts-react-frontend/src/components/pages/Pickit/scoreboard.tsx
@@ -3,8 +3,8 @@ import './scoreboard.css';
 import { RouteComponentProps } from 'react-router-dom';
 
 const Final_Round = 2;
-const Round1_Time = 1800;
-const Round2_Time = 1200;
+const Round1_Time = 180000;
+const Round2_Time = 120000;
 
 interface IScoreboardState {
     round: number,
@@ -56,21 +56,20 @@ class Scoreboard extends Component<RouteComponentProps, IScoreboardState> {
 
     refreshBoard() {
         let remainedTime: number = 0;
-        if(this.state.targetTime) {
+        if (this.state.targetTime) {
             remainedTime = this.state.targetTime.getTime() - Date.now();
         } else {
             alert('Time not set error');
             return;
         }
-        console.log('refresh!!: ' + remainedTime);
 
-        if(remainedTime > 0) {
+        if (remainedTime > 0) {
             switch (this.state.turn) {
                 case GameTurn.P1:
-                    this.setState({p1Time: new Date(remainedTime)});
+                    this.setState({ p1Time: new Date(remainedTime) });
                     break;
                 case GameTurn.P2:
-                    this.setState({p2Time: new Date(remainedTime)});
+                    this.setState({ p2Time: new Date(remainedTime) });
                     break;
                 case GameTurn.End:
                     alert('게임을 초기화했습니다.');
@@ -79,11 +78,27 @@ class Scoreboard extends Component<RouteComponentProps, IScoreboardState> {
                     alert('Unknown Turn error');
                     return;
             }
-            
+
             setTimeout(this.refreshBoard, 500);
         } else {    // 게임 각 턴 종료
             let ding = new Audio('sound/page-pickit-1.mp3');
             ding.play();
+            setTimeout(() => {
+                ding = new Audio('sound/page-pickit-1.mp3');
+                ding.play();
+            }, 500);
+            setTimeout(() => {
+                ding = new Audio('sound/page-pickit-1.mp3');
+                ding.play();
+            }, 1000);
+            setTimeout(() => {
+                ding = new Audio('sound/page-pickit-1.mp3');
+                ding.play();
+            }, 1500);
+            setTimeout(() => {
+                ding = new Audio('sound/page-pickit-1.mp3');
+                ding.play();
+            }, 2000);   //5번 울리기
 
             let temp: GameTurn = this.state.turn + 1
             if (temp >= GameTurn.End) {
@@ -126,36 +141,33 @@ class Scoreboard extends Component<RouteComponentProps, IScoreboardState> {
     render() {
         return (
             <div className="score-app">
-                <div className="time-board">
-                    <div className="time-content">
-                        <span className="score-text">
-                            {this.state.p1Time.getMinutes() + ":" +
-                                (this.state.p1Time.getSeconds() < 10 ? "0" + this.state.p1Time.getSeconds() : this.state.p1Time.getSeconds())}
+                <div className="scoreboard">
+                    <div className="score">
+                        <span className="score-app-text">
+                            <input type="text" onChange={this.onChangeP1Score} value={this.state.p1Score} />
                         </span>
                     </div>
                     <div className="round">
                         <span className="round-title">Round</span>
-                        <span className="round-content">{this.state.round > Final_Round? "종료" : this.state.round}</span>
+                        <span className="round-content">{this.state.round > Final_Round ? "종료" : this.state.round}</span>
                     </div>
-                    <div className="time-content">
-                        <span className="score-text">
-                            {this.state.p2Time.getMinutes() + ":" +
-                                (this.state.p2Time.getSeconds() < 10 ? "0" + this.state.p2Time.getSeconds() : this.state.p2Time.getSeconds())}
+                    <div className="score">
+                        <span className="score-app-text">
+                            <input type="text" onChange={this.onChangeP2Score} value={this.state.p2Score} />
                         </span>
                     </div>
                 </div>
-                <div className="score-board">
-                    <div className="score-content">
-                        <span className="score-text">
-                            <input type="text" onChange={this.onChangeP1Score} value={this.state.p1Score} />
+                <div className="time-board">
+                    <div className="time">
+                        <span className="score-app-text">
+                            {this.state.p1Time.getMinutes() + ":" +
+                                (this.state.p1Time.getSeconds() < 10 ? "0" + this.state.p1Time.getSeconds() : this.state.p1Time.getSeconds())}
                         </span>
                     </div>
-                    <div className="divider">
-                        <span className="score-text">:</span>
-                    </div>
-                    <div className="score-content">
-                        <span className="score-text">
-                            <input type="text" onChange={this.onChangeP2Score} value={this.state.p2Score} />
+                    <div className="time">
+                        <span className="score-app-text">
+                            {this.state.p2Time.getMinutes() + ":" +
+                                (this.state.p2Time.getSeconds() < 10 ? "0" + this.state.p2Time.getSeconds() : this.state.p2Time.getSeconds())}
                         </span>
                     </div>
                 </div>
